96ff263e76575e5daf5bb01ff9a4209d8d16c904
jdbernardo16 <jdbernardo16@gmail.com>
2022-07-23 13:05:32 +0800

    initial init

--- vendor/bummzack/sortablefile/client/src/state/SortableUploadFieldActions.js
+++
@@ -1,9 +0,0 @@
-import ACTION_TYPES from './SortableUploadFieldActionTypes';
-
-export function changeSort(fieldId, oldIndex, newIndex) {
-  return (dispatch) =>
-    dispatch({
-      type: ACTION_TYPES.SORTABLE_UPLOADFIELD_SORT,
-      payload: {fieldId, oldIndex, newIndex},
-    });
-}
